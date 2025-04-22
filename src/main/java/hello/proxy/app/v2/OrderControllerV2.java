7270edfcaa0273e9cb436761466ee9c540f97c3f
Teo_You <teoyou881@gmail.com>
2025-04-21 23:44:22 -0400

    apply jdk dynamic proxy

--- src/main/java/hello/proxy/app/v2/OrderControllerV2.java
+++ src/main/java/hello/proxy/app/v2/OrderControllerV2.java
@@ -3,6 +3,7 @@ package hello.proxy.app.v2;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +18,7 @@ public class OrderControllerV2 {
   }
 
   @GetMapping("/v2/request")
-  public String request(String itemId) {
+  public String request(@RequestParam("itemId") String itemId) {
     orderService.orderItem(itemId);
     return "ok";
   }
